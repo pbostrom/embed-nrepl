a9c5d763709dd485a12667c6a0704f5f8063cd87
Paul Bostrom <pbostrom@roomkey.com>
2014-06-18 16:29:26 -0400

    create custom handler

--- src/embed_nrepl/core.clj
+++ src/embed_nrepl/core.clj
@@ -1,7 +1,28 @@
 (ns embed-nrepl.core
-  (:require [clojure.tools.nrepl.server :as nrepl]))
+  (:require [clojure.tools.nrepl.server :as nrepl]
+            [cider.nrepl.middleware.classpath]
+            [cider.nrepl.middleware.complete]
+            [cider.nrepl.middleware.info]
+            [cider.nrepl.middleware.inspect]
+            [cider.nrepl.middleware.macroexpand]
+            [cider.nrepl.middleware.resource]
+            [cider.nrepl.middleware.stacktrace]
+            [cider.nrepl.middleware.test]
+            [cider.nrepl.middleware.trace]))
+
+(defn cider-handler []
+  (apply nrepl/default-handler
+         [cider.nrepl.middleware.classpath/wrap-classpath
+          cider.nrepl.middleware.complete/wrap-complete
+          cider.nrepl.middleware.info/wrap-info
+          cider.nrepl.middleware.inspect/wrap-inspect
+          cider.nrepl.middleware.macroexpand/wrap-macroexpand
+          cider.nrepl.middleware.resource/wrap-resource
+          cider.nrepl.middleware.stacktrace/wrap-stacktrace
+          cider.nrepl.middleware.test/wrap-test
+          cider.nrepl.middleware.trace/wrap-trace]))
 
 (defn -main
   []
-  (nrepl/start-server :port 7888)
+  (nrepl/start-server :port 7888 :handler (cider-handler))
   (println  "nrepl started on port 7888"))
